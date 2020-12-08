53fa27fe07034b59b6d2b55d42da3717efaae0a7
Ankush <ankushmishra0939@gmail.com>
2020-12-08 11:09:15 +0530

    Working

--- app.js
+++ app.js
@@ -26,47 +26,56 @@ app.get('/', (req, res) => {
 });
 
 app.post('/send', (req,res) => {
-    const output = `
-    <p> You have a new contact request</p>
+    const output = 
+    `<p> You have a new contact request</p>
     <h3>Contact details</h3>
     <u>
       <li>Name: $(req.body.name)</li>
-      <li>Company: $(req.body.conpany)</li>
+      <li>Company: $(req.body.company)</li>
       <li>Email: $(req.body.email)</li>
       <li>Phone: $(req.body.phone)</li>
     </u>  
     <h3>Message</h3>
-    <li> $(req.body.message)</li>
-    `
+    <li> $(req.body.message)</li>`
+    
     console.log(req.body);
 
-    let transporter = nodemailer.createTransport({
-        host: "mail.smtp2go.com",
-        port: 587,
-        secure: false, // true for 465, false for other ports
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        //port: 587,
+        //secure: false, // true for 465, false for other ports
         auth: {
-          user: 'ankushmishra0939@gmail.com', // generated ethereal user
-          pass: '', // generated ethereal password
+          user: '', //Set your sender email address
+          pass: '', //Give your password for uuthentication
         },
-        tls: {
+        /*tls: {
             rejectUnauthorized: false
+        }*/
+      });
+
+      var mailoption = {
+        from: 'test.ankush.node@gmail.com', // sender address
+        to: req.body.email, // list of receivers
+        subject: "Node Contact Test", // Subject line
+        //text: "Hello world?", // plain text body
+        html: output, // html body
+      }
+    
+      // send mail with defined transport object
+      transporter.sendMail(mailoption, (err) => {
+        if(err) {
+          console.log(err);
+        }
+        else {
+          console.log('Email Sent');
         }
       });
     
-      // send mail with defined transport object
-      let info = transporter.sendMail({
-        from: '"Ankush &apos s Node Mailer" <ankushmishra0939@gmail.com>', // sender address
-        to: "req.body.email", // list of receivers
-        subject: "Node Contact Test", // Subject line
-        text: "Hello world?", // plain text body
-        html: output, // html body
-      });
-    
-      console.log("Message sent: %s", info.messageId);
+      //console.log("Message sent: %s", info.messageId);
       // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
     
       // Preview only available when sending through an Ethereal account
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+      //console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
       // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 
       res.render('contact', {msg: 'mail has been sent'});
